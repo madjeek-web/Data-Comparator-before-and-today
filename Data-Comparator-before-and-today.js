d5949ee16a4e149d446ea6ed4371055794656826
Fabien Conéjéro <free-email-contact@protonmail.com>
2024-01-29 22:07:41 +0100

    Add files via upload

---
+++ Data-Comparator-before-and-today.js
@@ -0,0 +1,26 @@
+
+//**************************************************************
+// MIT PROJECT - Free license - (c) Fabien Conéjéro 2024 / FC84
+// Data Comparator: before and today
+// + search system of information for users
+// values and data can be added to this simple baseline
+//**************************************************************
+
+    function Year2023(type, quantity) {
+      this.type = type;
+      this.quantity = quantity;
+    }
+    
+    var year1917 = new Year2023('5 482', 'exoplanet');
+    
+    var year1917 = {
+      quantity: "1 ?",
+      type: "exoplanet",
+    };
+    
+    var Year2023 = {
+      quantity: "5 482",
+      type: "exoplanet",
+    };
+    
+    console.log(year1917.quantity);
